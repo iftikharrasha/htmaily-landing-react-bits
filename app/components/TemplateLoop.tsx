417c782a7a4eb945774cadc9d24e30e4a62a85de
iftikhar rasha <iftikhar.rasha@northsouth.edu>
2026-02-11 15:07:59 +0600

    first commit

---
+++ app/components/TemplateLoop.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import LogoLoop from "@/components/LogoLoop"
+import Image from "next/image"
+
+type TemplateItem = {
+  src: string
+  alt: string
+}
+
+const templates: TemplateItem[] = Array.from({ length: 10 }, (_, i) => ({
+  src: "/preview.png",
+  alt: `Template ${i + 1}`,
+}))
+
+export default function TemplateLoop() {
+  return (
+    <section className="relative bg-black text-white py-16 overflow-hidden">
+      <div className="relative mx-auto w-full px-4 md:px-8 lg:px-10">
+        {/* Header */}
+        <div className="mb-12">
+          <h2
+            className="text-4xl font-bold text-[#FAFAFA]"
+          >
+            Explore All Templates
+          </h2>
+        </div>
+
+        {/* Infinite loop template showcase */}
+        <LogoLoop
+            logos={templates}
+            speed={80}
+            hoverSpeed={20}
+            direction="left"
+            gap={32}
+            logoHeight={368}
+            fadeOut
+            fadeOutColor="#000000"
+            ariaLabel="Template previews"
+            renderItem={(item) => (
+            <div className="relative shrink-0 pt-4">
+                <div
+                    className="
+                        group
+                        relative
+                        w-90
+                        cursor-pointer
+                        transition-transform duration-300 ease-out
+                        will-change-transform
+                        hover:-translate-y-3 z-10
+                    "
+                >
+                    <div className="relative rounded-xl shadow-2xl">
+                        <Image
+                            src={item.src}
+                            alt={item.alt}
+                            width={368}
+                            height={460}
+                            className="
+                                w-full h-auto object-cover
+                                opacity-70
+                                transition-opacity duration-300
+                                hover:opacity-100
+                            "
+                            loading="lazy"
+                        />
+                    </div>
+                </div>
+            </div>
+            )}
+        />
+      </div>
+    </section>
+  )
+}
